790a00e293df173d3a6e5dd76433db0f6ca24afb
Boris Klco <klco@pm.me>
2023-06-19 16:53:42 +0000

    Burger flipper

--- src/List.jsx
+++ src/List.jsx
@@ -12,13 +12,26 @@ const List = () => {
   if (contactList.isLoading) return "Loading...";
 
   return (
-    <div className="list">
-      {contactList.map((contact) => (
-        <div key={contact.id} className="listItem">
-          <p>{contact.first_name}</p>
-          <p>{contact.last_name}</p>
-        </div>
-      ))}
+    <div className="listContainer">
+      <div className="listBody">
+        {contactList.map((contact) => (
+          <div key={contact.id} className="listItem">
+            <p className="gender">{contact.gender}</p>
+            <p className="name">
+              {contact.first_name} {contact.last_name}
+            </p>
+            <p className="user">{contact.username}</p>
+            <p className="email">{contact.email}</p>
+            <input
+              className="title"
+              type="text"
+              value={contact.employment.title}
+              disabled
+            ></input>
+            {/*  <p className="title">{contact.employment.title}</p> */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
