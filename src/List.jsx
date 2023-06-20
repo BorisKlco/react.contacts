4477ef5ce562356637a7c24daddb112cc98d1619
Boris Klco <klco@pm.me>
2023-06-20 08:37:16 +0000

    basic card

--- src/List.jsx
+++ src/List.jsx
@@ -31,9 +31,68 @@ const List = () => {
               ></input>
             </div>
             <div className="itemExtended">
-              <p>asdasd</p>
-              <p>asdasd</p>
-              <p>asdasd</p>
+              <div className="left">
+                <img
+                  src={contact.avatar}
+                  alt={contact.id}
+                  data-color={
+                    contact.id < 1500
+                      ? "green"
+                      : contact.id < 3500
+                      ? "yellow"
+                      : contact.id < 5000
+                      ? "blue"
+                      : "red"
+                  }
+                />
+                <h2>
+                  {contact.first_name} <span>{contact.last_name}</span>
+                </h2>
+                <h3>{contact.employment.title}</h3>
+              </div>
+              <div className="middle">
+                <div className="personal">
+                  <h2>Personal Info</h2>
+                  <p>
+                    ID: <span>#{contact.id}</span>
+                  </p>
+                  <p>
+                    Nick: <span>{contact.username}</span>
+                  </p>
+                  <p>
+                    Email: <span>{contact.email}</span>
+                  </p>
+                  <p>
+                    Birth: <span>{contact.date_of_birth}</span>
+                  </p>
+                </div>
+                <div className="address">
+                  <h2>Address</h2>
+                  <p>
+                    State: <span>{contact.address.state}</span>
+                  </p>
+                  <p>
+                    City: <span>{contact.address.city}</span>
+                  </p>
+                  <p>
+                    Street: <span>{contact.address.street_name}</span>
+                  </p>
+                </div>
+              </div>
+              <div className="right">
+                <h2>Subscription</h2>
+                <div>
+                  <p>
+                    Plan: <span>{contact.subscription.plan}</span>
+                  </p>
+                  <p>
+                    Status: <span>{contact.subscription.status}</span>
+                  </p>
+                  <p>
+                    Payment: <span>{contact.subscription.payment_method}</span>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         ))}
