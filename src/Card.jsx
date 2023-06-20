464f5113b2dc1dfc84ddfe05e64b600afff05305
Boris Klco <klco@pm.me>
2023-06-20 10:18:05 +0000

    more css than react.. ok burger flipper

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,101 @@
-const Card = () => {
+const Card = ({ contact }) => {
   return (
-    <div>
-      <h1>Card</h1>
+    <div className="listItem">
+      <div className="itemInfo">
+        <p className="id">#{contact.id}</p>
+        <p className="name">
+          {contact.first_name} {contact.last_name}
+        </p>
+        <p className="user">{contact.username}</p>
+        <p className="email">{contact.email}</p>
+        <input
+          className="title"
+          type="text"
+          value={contact.employment.title}
+          disabled
+        ></input>
+      </div>
+      <div className="itemExtended">
+        <div className="left">
+          <img
+            src={contact.avatar}
+            alt={contact.id}
+            data-color={
+              contact.id < 1500
+                ? "green"
+                : contact.id < 3500
+                ? "yellow"
+                : contact.id < 5000
+                ? "blue"
+                : "red"
+            }
+          />
+          <h2>
+            {contact.first_name} <span>{contact.last_name}</span>
+          </h2>
+          <h3>{contact.employment.title}</h3>
+        </div>
+        <div className="middle">
+          <div className="personal">
+            <h2 className="padding">Personal Info</h2>
+            <p>
+              ID: <span>#{contact.id}</span>
+            </p>
+            <p>
+              Nick: <span>{contact.username}</span>
+            </p>
+            <p>
+              Email: <span>{contact.email}</span>
+            </p>
+            <p>
+              Birth: <span>{contact.date_of_birth}</span>
+            </p>
+          </div>
+          <div className="address">
+            <h2 className="padding">Address</h2>
+            <p>
+              State: <span>{contact.address.state}</span>
+            </p>
+            <p>
+              City: <span>{contact.address.city}</span>
+            </p>
+            <p>
+              Street: <span>{contact.address.street_name}</span>
+            </p>
+          </div>
+        </div>
+        <div
+          className="right"
+          data-rank={
+            contact.subscription.plan === "Diamond"
+              ? "dia"
+              : contact.subscription.plan === "Silver"
+              ? "silver"
+              : contact.subscription.plan === "Gold"
+              ? "gold"
+              : contact.subscription.plan === "Bronze"
+              ? "bronze"
+              : contact.subscription.plan === "Starter"
+              ? "starter"
+              : contact.subscription.plan === "Professional"
+              ? "professional"
+              : "others"
+          }
+        >
+          <h2 className="padding">Subscription</h2>
+          <div>
+            <p>
+              Plan: <span>{contact.subscription.plan}</span>
+            </p>
+            <p>
+              Status: <span>{contact.subscription.status}</span>
+            </p>
+            <p>
+              Payment: <span>{contact.subscription.payment_method}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/List.jsx
+++ src/List.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import Card from "./Card";
 import fetchContacs from "./fetchContacs";
 
 const List = () => {
@@ -13,88 +14,10 @@ const List = () => {
 
   return (
     <div className="listContainer">
+      <h1 className="hover">Hover Over Contact</h1>
       <div className="listBody">
         {contactList.map((contact) => (
-          <div key={contact.id} className="listItem">
-            <div className="itemInfo">
-              <p className="id">#{contact.id}</p>
-              <p className="name">
-                {contact.first_name} {contact.last_name}
-              </p>
-              <p className="user">{contact.username}</p>
-              <p className="email">{contact.email}</p>
-              <input
-                className="title"
-                type="text"
-                value={contact.employment.title}
-                disabled
-              ></input>
-            </div>
-            <div className="itemExtended">
-              <div className="left">
-                <img
-                  src={contact.avatar}
-                  alt={contact.id}
-                  data-color={
-                    contact.id < 1500
-                      ? "green"
-                      : contact.id < 3500
-                      ? "yellow"
-                      : contact.id < 5000
-                      ? "blue"
-                      : "red"
-                  }
-                />
-                <h2>
-                  {contact.first_name} <span>{contact.last_name}</span>
-                </h2>
-                <h3>{contact.employment.title}</h3>
-              </div>
-              <div className="middle">
-                <div className="personal">
-                  <h2>Personal Info</h2>
-                  <p>
-                    ID: <span>#{contact.id}</span>
-                  </p>
-                  <p>
-                    Nick: <span>{contact.username}</span>
-                  </p>
-                  <p>
-                    Email: <span>{contact.email}</span>
-                  </p>
-                  <p>
-                    Birth: <span>{contact.date_of_birth}</span>
-                  </p>
-                </div>
-                <div className="address">
-                  <h2>Address</h2>
-                  <p>
-                    State: <span>{contact.address.state}</span>
-                  </p>
-                  <p>
-                    City: <span>{contact.address.city}</span>
-                  </p>
-                  <p>
-                    Street: <span>{contact.address.street_name}</span>
-                  </p>
-                </div>
-              </div>
-              <div className="right">
-                <h2>Subscription</h2>
-                <div>
-                  <p>
-                    Plan: <span>{contact.subscription.plan}</span>
-                  </p>
-                  <p>
-                    Status: <span>{contact.subscription.status}</span>
-                  </p>
-                  <p>
-                    Payment: <span>{contact.subscription.payment_method}</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Card contact={contact} key={contact.uid} />
         ))}
       </div>
     </div>
